fd30c701b0efe4cb264c9fc171ec3ec7cc377fa9
sarulmozhi <36985417+sarulmozhi@users.noreply.github.com>
2018-07-24 15:58:44 +0530

    Create magizh10.py

---
+++ magizh10.py
@@ -0,0 +1,11 @@
+num1=input("enter number"):
+num2=input("enter number"):
+num3=input("enter number"):
+elif:
+  if((num1>=num2) and (num1>=num3)):
+     larger is num1
+elif:
+    if((num2>=num1) and (num2>=num3)):
+      larger is num2
+else:
+    num3
