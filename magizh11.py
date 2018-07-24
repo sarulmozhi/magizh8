c8bd99d7dd343efa98a0194b8e358f0e0bbc7c56
sarulmozhi <36985417+sarulmozhi@users.noreply.github.com>
2018-07-24 16:14:41 +0530

    Update magizh11.py

---
+++ magizh11.py
@@ -0,0 +1,11 @@
+year=input("enter number"):
+ if (year % 4)==0
+   if (year % 100)==0
+       if (year % 400)==0
+          print({0} "is an leap year:".format(year))
+  else:
+          print({0} "is not an leap year:".format(year))
+     else:
+             print({0} "is not an leap year:".format(year))
+         else:
+                  print({0} "is not an leap year:".format(year))
